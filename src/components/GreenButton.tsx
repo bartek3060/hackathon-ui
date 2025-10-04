acccdc2a1ab50edba2574daf5a048bd80c7669c5
Bartek Lesniewski <bartek30603060@onet.pl>
2025-10-04 17:26:27 +0200

    a

--- src/components/GreenButton.tsx
+++ src/components/GreenButton.tsx
@@ -1,12 +1,13 @@
 import { Button } from "@/components/ui/button";
-import { type ButtonProps } from "@/components/ui/button";
+import { ButtonProps } from "react-day-picker";
 
 export function GreenButton({ className, ...props }: ButtonProps) {
   return (
     <Button
-      className={`bg-[rgb(0,153,63)] hover:bg-[rgb(0,133,53)] text-white transition-colors duration-200 ${className || ""}`}
+      className={`bg-[rgb(0,153,63)] hover:bg-[rgb(0,133,53)] text-white transition-colors duration-200 ${
+        className || ""
+      }`}
       {...props}
     />
   );
 }
-
