f8a302cecd7cfac2ee54fc9253109c2b277991c4
Bartek Lesniewski <bartek30603060@onet.pl>
2025-10-05 08:01:15 +0200

    a

---
+++ src/api/dtos/calculate-detailed-pension.ts
@@ -0,0 +1,16 @@
+export interface CalculateDetailedPensionDto {
+  sessionId: string;
+  gender: string;
+  birthDate: string;
+  grossSalary: number;
+  workStartYear: string;
+  plannedWorkEndYear: string;
+  amountOfMoneyInZusAccount: number;
+  amountOfMoneyInZusSubAccount: number;
+  includeSickLeave: boolean;
+  postalCode: string;
+  particularSalaries:{year:number, salary:number}[];
+  particularSickLeave:{year:number, days:number}[];
+  yearIndexation: number[]
+  shouldUsePreviousZusAmount: boolean;
+}
